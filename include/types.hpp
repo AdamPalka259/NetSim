629cda7f4d73fc561c0135658d5eea9d3d791589
Adam Palka <adam.palka259@gmail.com>
2023-12-22 23:33:44 +0100

    Poprawienie types.hpp; aktualizacja CMakeLists.txt

--- include/types.hpp
+++ include/types.hpp
@@ -3,8 +3,8 @@
 #include <functional>
 
 using ElementID = int;
-using TimeOffset = unsigned int;
-using Time = unsigned int;
+using TimeOffset = int;
+using Time = int;
 using ProbabilityGenerator = std::function<double()>;
 
 #endif //NETSIM_TYPES_HPP
