0dd0f4e161e501ba9e3d48b413df26452d8a08b6
Adam Palka <adam.palka259@gmail.com>
2024-01-02 23:46:30 +0100

    Przywrócenie simulation.cpp

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -4,16 +4,6 @@
 #include "factory.hpp"
 #include "types.hpp"
 
-void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)> rf){
-    for(Time t = 1; t <= d; t++){
-        if (!f.is_consistent()){
-            throw std::logic_error("Fabryka nie jest spójna");
-        }
-        f.do_deliveries(t);
-        f.do_package_passing();
-        f.do_work(t);
-        rf(f,t);
-    }
-}
+void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)> rf);
 
 #endif //NETSIM_SIMULATION_HPP
